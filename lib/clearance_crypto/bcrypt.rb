8d89ff40d89c16fbf094d7c4597e34c07f1ec304
Michael Moen <michael@underpantsgnome.com>
2009-02-17 15:37:48 -0600

    Add salt to bcrypt

--- lib/clearance_crypto/bcrypt.rb
+++ lib/clearance_crypto/bcrypt.rb
@@ -14,12 +14,12 @@ module ClearanceCrypto
 
       # Creates a BCrypt hash for the password passed.
       def encrypt(string)
-        ::BCrypt::Password.create(string)
+        ::BCrypt::Password.create("--#{salt}--#{string}--")
       end
 
       # Does the hash match the tokens? Uses the same tokens that were used to encrypt.
       def authenticated?(string)
-        ::BCrypt::Password.new(self.encrypted_password) == string rescue false
+        ::BCrypt::Password.new(self.encrypted_password) == "--#{salt}--#{string}--" rescue false
       end
     end
   end
